ebb1abb084c4dc9c48147aa4b95cba45b27ee617
TJ Stifter <tjstifter@gmail.com>
2023-07-03 16:14:28 -0400

    completes lab

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,23 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while (i > 0) :
+        print(i)
+        i = i - 1
+    print("Happy New Year!")    
+
 
 def square_integers(int_list):
     # code goes here!
-    pass
-
+    square_list = [(integer * integer) for integer in int_list]
+    return square_list
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1,101) : 
+        if(i % 15 == 0) : print("FizzBuzz")
+        elif (i % 3 == 0) : print("Fizz")
+        elif (i % 5 == 0) : print("Buzz")
+        else : print(i)
+
+fizzbuzz()
